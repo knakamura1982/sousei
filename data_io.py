316b179946c3ad2cb6aad0ff6874f870b0454036
knakamura1982 <63708845+knakamura1982@users.noreply.github.com>
2021-03-19 12:58:06 +0900

    Add files via upload

--- data_io.py
+++ data_io.py
@@ -1,9 +1,11 @@
+import os
 import csv
 import cv2
 import numpy as np
 
 
 # CSV形式で記載されたデータファイルを読み込む
+# 左端の列にクラスラベルが，残りの列に特徴ベクトルが記載されていることを想定
 #   - filename: データファイルのファイルパス
 #   - with_header: ヘッダ行が存在するか否か
 #   - dic: クラスラベル名からクラス番号への対応を表す辞書（Noneの場合は自動作成）
@@ -59,8 +61,10 @@ def read_features(filename, with_header=True, dic=None):
 #   - filename: 画像リストファイルのファイルパス
 #   - data_dir: 画像ファイル名の先頭に付加する接頭辞（画像フォルダ名）
 #   - with_header: ヘッダ行が存在するか否か
+#   - cid_lab: 何列目にクラスラベルが記載されているかを指定する整数値（0-based index, デフォルト値: 0）
+#   - cid_dat: 何列目に画像ファイル名が記載されているかを指定する整数値（0-based index, デフォルト値: 1）
 #   - dic: クラスラベル名からクラス番号への対応を表す辞書（Noneの場合は自動作成）
-def read_image_list(filename, data_dir='./', with_header=True, dic=None):
+def read_image_list(filename, data_dir='./', with_header=True, cid_lab=0, cid_dat=1, dic=None):
 
     # CSVファイルを開く
     f = open(filename, 'r')
@@ -79,19 +83,19 @@ def read_image_list(filename, data_dir='./', with_header=True, dic=None):
         nc = 0
         dic = {}
         for row in reader:
-            if not row[0] in dic:
-                name.append(row[0])
-                dic[row[0]] = nc
+            if not row[cid_lab] in dic:
+                name.append(row[cid_lab])
+                dic[row[cid_lab]] = nc
                 nc += 1
-            lab.append(dic[row[0]])
-            img.append(data_dir + row[1])
+            lab.append(dic[row[cid_lab]])
+            img.append(os.path.join(data_dir, row[cid_dat]))
     else:
         # クラスラベル名とクラス番号の対応関係が既知の場合
         for row in reader:
-            if not row[0] in dic:
+            if not row[cid_lab] in dic:
                 continue
-            lab.append(dic[row[0]])
-            img.append(data_dir + row[1])
+            lab.append(dic[row[cid_lab]])
+            img.append(os.path.join(data_dir, row[cid_dat]))
 
     # 読み込み結果を numpy.ndarray 形式に変換
     lab = np.asarray(lab)
@@ -132,16 +136,34 @@ def add_noise(img, mode, num):
 # 単一の画像を読み込む
 #   - filename: 画像ファイルのファイルパス
 #   - size: 画像サイズを強制的に size = (width, height) に変更する（size == None のときは無視される）
+#   - pad: 画像サイズの強制変更をパディングで実現するか否か（pad == True ならパディングで，pad == False なら単純な伸縮でサイズを変更する．size == None のときは無視される）
 #   - mode: 読み込みモード（0: グレースケール画像モード，それ以外: カラー画像モード）
 #   - with_noise: True なら強制的にごま塩ノイズを追加して読み込む
 #   - n_noise_points: ごま塩ノイズとして追加する点の個数（with_noise == False のときは無視される）
-def load_single_image(filename, size=None, mode=1, with_noise=False, n_noise_points=100):
+def load_single_image(filename, size=None, pad=False, mode=1, with_noise=False, n_noise_points=100):
 
     if mode == 0:
         # グレースケール画像モード
         img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # グレースケール画像として読み込む
         if not size is None:
-            img = cv2.resize(img, dsize=size, interpolation=cv2.INTER_NEAREST)
+            if pad:
+                dw = img.shape[1] - size[0]
+                dh = img.shape[0] - size[1]
+                dwh = dw // 2
+                dhh = dh // 2
+                if dw < 0:
+                    temp = img[0, 0] * np.ones((img.shape[0], size[0]), dtype=img.dtype)
+                    temp[ : , -dwh : -dwh + img.shape[1]] = img
+                    dw = dwh = 0
+                    img = temp
+                if dh < 0:
+                    temp = img[0, 0] * np.ones((size[1], img.shape[1]), dtype=img.dtype)
+                    temp[-dhh : -dhh + img.shape[0], : ] = img
+                    dh = dhh = 0
+                    img = temp
+                img = img[dhh : dhh + size[1], dwh : dwh + size[0]]
+            else:
+                img = cv2.resize(img, dsize=size, interpolation=cv2.INTER_NEAREST)
         if with_noise:
             img = add_noise(img, mode, n_noise_points)
         img = img.reshape((1, img.shape[0], img.shape[1])) # チャンネル数 1 の三次元テンソルとなるように変形
@@ -150,7 +172,24 @@ def load_single_image(filename, size=None, mode=1, with_noise=False, n_noise_poi
         # カラー画像モード
         img = cv2.imread(filename, cv2.IMREAD_COLOR) # カラー画像として読み込む
         if not size is None:
-            img = cv2.resize(img, dsize=size, interpolation=cv2.INTER_NEAREST)
+            if pad:
+                dw = img.shape[1] - size[0]
+                dh = img.shape[0] - size[1]
+                dwh = dw // 2
+                dhh = dh // 2
+                if dw < 0:
+                    temp = np.asarray([[img[0, 0]]]) * np.ones((img.shape[0], size[0], img.shape[2]), dtype=img.dtype)
+                    temp[ : , -dwh : -dwh + img.shape[1], : ] = img
+                    dw = dwh = 0
+                    img = temp
+                if dh < 0:
+                    temp = np.asarray([[img[0, 0]]]) * np.ones((size[1], img.shape[1], img.shape[2]), dtype=img.dtype)
+                    temp[-dhh : -dhh + img.shape[0], : , : ] = img
+                    dh = dhh = 0
+                    img = temp
+                img = img[dhh : dhh + size[1], dwh : dwh + size[0]]
+            else:
+                img = cv2.resize(img, dsize=size, interpolation=cv2.INTER_NEAREST)
         if with_noise:
             img = add_noise(img, mode, n_noise_points)
         img = img.transpose(2, 0, 1) # (チャンネル数，縦幅，横幅) の順となるように変形
@@ -160,25 +199,26 @@ def load_single_image(filename, size=None, mode=1, with_noise=False, n_noise_poi
 
 
 # 画像データを読み込む
-#   - image_list: 画像ファイルパ名のリスト（関数 read_image_list の出力を指定する）
+#   - image_list: 画像ファイル名のリスト（関数 read_image_list の出力を指定する）
 #   - ids: リスト中の何番目の画像を読み込むかを指定する整数配列
 #   - size: 画像サイズを強制的に size = (width, height) に変更する（size == None のときは無視される）
+#   - pad: 画像サイズの強制変更をパディングで実現するか否か（pad == True ならパディングで，pad == False なら単純な伸縮でサイズを変更する．size == None のときは無視される）
 #   - mode: 読み込みモード（0: グレースケール画像モード，それ以外: カラー画像モード）
 #   - with_noise: True なら強制的にごま塩ノイズを追加して読み込む
 #   - n_noise_points: ごま塩ノイズとして追加する点の個数（with_noise == False のときは無視される）
-def load_images(image_list, ids=None, size=None, mode=1, with_noise=False, n_noise_points=100):
+def load_images(image_list, ids=None, size=None, pad=False, mode=1, with_noise=False, n_noise_points=100):
    
     # 画像をロード
     n = 0
     images = [] # 読み込んだ画像の集合
     if ids is None:
         for i in range(0, len(image_list)):
-            img = load_single_image(image_list[i], size, mode, with_noise, n_noise_points) # i番目の画像を読み込む
+            img = load_single_image(image_list[i], size, pad, mode, with_noise, n_noise_points) # i番目の画像を読み込む
             images.append(img) # 画像を配列に追加
             n += 1 # 読み込んだ画像の総数をカウントしておく
     else:
         for i in range(0, len(ids)):
-            img = load_single_image(image_list[ids[i]], size, mode, with_noise, n_noise_points) # ids[i]番目の画像を読み込む
+            img = load_single_image(image_list[ids[i]], size, pad, mode, with_noise, n_noise_points) # ids[i]番目の画像を読み込む
             images.append(img) # 画像を配列に追加
             n += 1 # 読み込んだ画像の総数をカウントしておく
 
@@ -187,3 +227,32 @@ def load_images(image_list, ids=None, size=None, mode=1, with_noise=False, n_noi
 
     # 結果を返却
     return images
+
+
+# ラベルデータを one-hot vector 形式に展開する
+#   - label_list: ラベルデータのリスト（関数 read_image_list の出力を指定する）
+#   - ids: リスト中の何番目のデータを展開するかを指定する整数配列
+#   - n_classes: ラベルの種類数の総数（0以下の場合は自動計算）
+def load_labels(label_list, ids=None, n_classes=0):
+
+    if n_classes <= 0:
+        n_classes = len(np.unique(label_list))
+
+    n = 0
+    labels = []
+    if ids is None:
+        for i in range(0, len(label_list)):
+            lab = np.zeros(n_classes, dtype=np.float32)
+            lab[label_list[i]] = 1.0
+            labels.append(lab)
+            n += 1
+    else:
+        for i in range(0, len(ids)):
+            lab = np.zeros(n_classes, dtype=np.float32)
+            lab[label_list[ids[i]]] = 1.0
+            labels.append(lab)
+            n += 1
+
+    labels = np.asarray(labels, dtype=np.float32)
+
+    return labels
--- unit_layers.py
+++ unit_layers.py
@@ -36,37 +36,56 @@ class Conv(nn.Module):
 
 
 # プーリングを行う層
-# カーネルサイズおよびストライド幅は 2 で固定
 #   - method: プーリング手法（'max'または'avg'のいずれか）
+#   - scale: カーネルサイズおよびストライド幅
 class Pool(nn.Module):
 
-    def __init__(self, method='max'):
+    def __init__(self, method='max', scale=2):
         super(Pool, self).__init__()
         if method == 'avg':
-            self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+            self.pool = nn.AvgPool2d(kernel_size=scale, stride=scale)
         elif method == 'max':
-            self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+            self.pool = nn.MaxPool2d(kernel_size=scale, stride=scale)
 
     def __call__(self, x):
         return self.pool(x)
 
 
+# Global Average/Max Poolingを行う層
+class GlobalPool(nn.Module):
+
+    def __init__(self, method='avg'):
+        super(GlobalPool, self).__init__()
+        self.method = method
+
+    def __call__(self, x):
+        size = x.size()
+        if self.method == 'avg':
+            pool = nn.AvgPool2d(kernel_size=(size[2], size[3]))
+        elif self.method == 'max':
+            pool = nn.MaxPool2d(kernel_size=(size[2], size[3]))
+        h = pool(x)
+        h = h.view(size[0], size[1])
+        return h
+
+
 # 畳込み + バッチ正規化 + 活性化関数を行う層
-# 畳込みにおけるカーネルサイズ，パディングサイズ，ストライド幅は，出力マップのサイズが入力マップのサイズの半分になるように自動決定する
+# 畳込みにおけるパディングサイズとストライド幅は，出力マップのサイズが入力マップのサイズの半分になるように自動決定する
 # 入力マップのサイズは偶数であるものと仮定する
 #   - in_channels: 入力マップのチャンネル数
 #   - out_channels: 出力マップのチャンネル数
+#   - kernel_size: カーネルサイズ（偶数のみ可）
 #   - do_bn: バッチ正規化を行うか否か（Trueなら行う，Falseなら行わない）
 #   - dropout_ratio: 0以外ならその割合でドロップアウト処理を実行
 #   - activation: 活性化関数
 class DownConv(nn.Module):
 
-    def __init__(self, in_channels, out_channels, do_bn=True, dropout_ratio=0, activation=F.relu):
+    def __init__(self, in_channels, out_channels, kernel_size=4, do_bn=True, dropout_ratio=0, activation=F.relu):
         super(DownConv, self).__init__()
         self.do_bn = do_bn
         self.dropout_ratio = dropout_ratio
         self.activation = activation
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=4, stride=2, padding=1)
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=kernel_size//2-1)
         if do_bn:
             self.bn = nn.BatchNorm2d(num_features=out_channels)
 
@@ -82,20 +101,21 @@ class DownConv(nn.Module):
 
 
 # 逆畳込み + バッチ正規化 + 活性化関数を行う層
-# 逆畳込みにおけるカーネルサイズ，パディングサイズ，ストライド幅は，出力マップのサイズが入力マップのサイズの 2 倍になるように自動決定する
+# 逆畳込みにおけるパディングサイズとストライド幅は，出力マップのサイズが入力マップのサイズの 2 倍になるように自動決定する
 #   - in_channels: 入力マップのチャンネル数
 #   - out_channels: 出力マップのチャンネル数
+#   - kernel_size: カーネルサイズ（偶数のみ可）
 #   - do_bn: バッチ正規化を行うか否か（Trueなら行う，Falseなら行わない）
 #   - dropout_ratio: 0以外ならその割合でドロップアウト処理を実行
 #   - activation: 活性化関数
 class UpConv(nn.Module):
 
-    def __init__(self, in_channels, out_channels, do_bn=True, dropout_ratio=0, activation=F.relu):
+    def __init__(self, in_channels, out_channels, kernel_size=4, do_bn=True, dropout_ratio=0, activation=F.relu):
         super(UpConv, self).__init__()
         self.do_bn = do_bn
         self.dropout_ratio = dropout_ratio
         self.activation = activation
-        self.deconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=4, stride=2, padding=1)
+        self.deconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=kernel_size//2-1)
         if do_bn:
             self.bn = nn.BatchNorm2d(num_features=out_channels)
 
@@ -105,6 +125,39 @@ class UpConv(nn.Module):
             h = self.bn(h) # バッチ正規化
         if not self.activation is None:
             h = self.activation(h) # 活性化関数
+        if self.dropout_ratio != 0:
+            h = nn.Dropout2d(p=self.dropout_ratio)(h) # ドロップアウト
+        return h
+
+
+# 畳込み + バッチ正規化 + 活性化関数 + Pixel Shuffle を行う層
+# 畳込みにおけるパディングサイズとストライド幅は，出力マップのサイズが入力マップのサイズの 2 倍になるように自動決定する
+#   - in_channels: 入力マップのチャンネル数
+#   - out_channels: 出力マップのチャンネル数
+#   - kernel_size: カーネルサイズ（奇数のみ可）
+#   - scale: Pixel Shuffle におけるスケールファクタ
+#   - do_bn: バッチ正規化を行うか否か（Trueなら行う，Falseなら行わない）
+#   - dropout_ratio: 0以外ならその割合でドロップアウト処理を実行
+#   - activation: 活性化関数
+class PixShuffle(nn.Module):
+
+    def __init__(self, in_channels, out_channels, scale=2, kernel_size=3, do_bn=True, dropout_ratio=0, activation=F.relu):
+        super(PixShuffle, self).__init__()
+        self.do_bn = do_bn
+        self.dropout_ratio = dropout_ratio
+        self.activation = activation
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels*scale*scale, kernel_size=kernel_size, stride=1, padding=kernel_size//2)
+        self.ps = nn.PixelShuffle(scale)
+        if do_bn:
+            self.bn = nn.BatchNorm2d(num_features=out_channels*scale*scale)
+
+    def __call__(self, x):
+        h = self.conv(x) # 畳み込み
+        if self.do_bn:
+            h = self.bn(h) # バッチ正規化
+        if not self.activation is None:
+            h = self.activation(h) # 活性化関数
+        h = self.ps(h) # pixel shuffle
         if self.dropout_ratio != 0:
             h = nn.Dropout2d(p=self.dropout_ratio)(h) # ドロップアウト
         return h
@@ -138,18 +191,96 @@ class FC(nn.Module):
         return h
 
 
-# Bottle-Neck型ResBlock層
-# 畳込みにおけるカーネルサイズ，パディングサイズ，ストライド幅は，出力マップが入力マップと同じサイズになるように自動決定する
+# 平坦化を行う層
+#   - return_size: 入力マップのサイズを返却するか否か（Trueなら返す，Falseなら返さない）
+class Flatten(nn.Module):
+
+    def __init__(self, return_size=False):
+        super(Flatten, self).__init__()
+        self.return_size = return_size
+
+    def __call__(self, x):
+        size = x.size()
+        h = x.view(size[0], -1)
+        if self.return_size:
+            return h, size
+        else:
+            return h
+
+
+# 特徴マップの再配置（平坦化の逆）を行う層
+class Reshape(nn.Module):
+
+    def __init__(self):
+        super(Reshape, self).__init__()
+
+    # size: 再配置後の特徴マップのサイズ（Flattenの戻り値が使用されることを想定）
+    def __call__(self, x, size):
+        h = x.reshape(size)
+        return h
+
+
+# Plain型ResBlock層
+# 畳込みにおけるパディングサイズとストライド幅は，出力マップが入力マップと同じサイズになるように自動決定する
 #   - in_channels: 入力マップのチャンネル数
 #   - out_channels: 出力マップのチャンネル数（0以下の時は自動的に in_channels と同じ値を設定）
-#   - mid_channels: 中間層のマップのチャンネル数（0以下の時は自動的に in_channels // 4 を設定）
+#   - kernel_size: カーネルサイズ（奇数のみ可）
 #   - do_bn: バッチ正規化を行うか否か（Trueなら行う，Falseなら行わない）
 #   - dropout_ratio: 0以外ならその割合でドロップアウト処理を実行
 #   - activation: 活性化関数
-class ResBlock(nn.Module):
+class ResBlockP(nn.Module):
 
-    def __init__(self, in_channels, out_channels=0, mid_channels=0, do_bn=True, dropout_ratio=0, activation=F.relu):
-        super(ResBlock, self).__init__()
+    def __init__(self, in_channels, out_channels=0, kernel_size=3, do_bn=True, dropout_ratio=0, activation=F.relu):
+        super(ResBlockP, self).__init__()
+        self.do_bn = do_bn
+        self.dropout_ratio = dropout_ratio
+        self.activation = activation
+        if out_channels <= 0:
+            out_channels = in_channels
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=1, padding=kernel_size//2)
+        self.conv2 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=1, padding=kernel_size//2)
+        if self.do_bn:
+            self.bn1 = nn.BatchNorm2d(num_features=in_channels)
+            self.bn2 = nn.BatchNorm2d(num_features=in_channels)
+        if in_channels != out_channels:
+            self.use_resconv = True
+            self.resconv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+            if self.do_bn:
+                self.resbn = nn.BatchNorm2d(num_features=out_channels)
+        else:
+            self.use_resconv = False
+
+    def __call__(self, x):
+        if self.do_bn:
+            h = self.activation(self.bn1(self.conv1(x))) # 畳込み＆バッチ正規化＆活性化関数
+            h = self.bn2(self.conv2(h)) # 畳込み＆バッチ正規化
+        else:
+            h = self.activation(self.conv1(x)) # 畳込み＆活性化関数
+            h = self.conv2(h) # 畳込み
+        h = self.activation(h + x) # 加算＆活性化関数
+        if self.use_resconv:
+            if self.do_bn:
+                h = self.activation(self.resbn(self.resconv(h))) # チャンネル数を変更するための畳込み＆バッチ正規化
+            else:
+                h = self.activation(self.resconv(h)) # チャンネル数を変更するための畳込み
+        if self.dropout_ratio != 0:
+            h = nn.Dropout2d(p=self.dropout_ratio)(h) # ドロップアウト
+        return h
+
+
+# Bottle-Neck型ResBlock層
+# 畳込みにおけるパディングサイズとストライド幅は，出力マップが入力マップと同じサイズになるように自動決定する
+#   - in_channels: 入力マップのチャンネル数
+#   - out_channels: 出力マップのチャンネル数（0以下の時は自動的に in_channels と同じ値を設定）
+#   - mid_channels: 中間層のマップのチャンネル数（0以下の時は自動的に in_channels // 4 を設定）
+#   - kernel_size: カーネルサイズ（奇数のみ可）
+#   - do_bn: バッチ正規化を行うか否か（Trueなら行う，Falseなら行わない）
+#   - dropout_ratio: 0以外ならその割合でドロップアウト処理を実行
+#   - activation: 活性化関数
+class ResBlockBN(nn.Module):
+
+    def __init__(self, in_channels, out_channels=0, mid_channels=0, kernel_size=3, do_bn=True, dropout_ratio=0, activation=F.relu):
+        super(ResBlockBN, self).__init__()
         self.do_bn = do_bn
         self.dropout_ratio = dropout_ratio
         self.activation = activation
@@ -158,7 +289,7 @@ class ResBlock(nn.Module):
         if mid_channels <= 0:
             mid_channels = in_channels // 4
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=1, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(in_channels=mid_channels, out_channels=mid_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=mid_channels, out_channels=mid_channels, kernel_size=kernel_size, stride=1, padding=kernel_size//2)
         self.conv3 = nn.Conv2d(in_channels=mid_channels, out_channels=in_channels, kernel_size=1, stride=1, padding=0)
         if self.do_bn:
             self.bn1 = nn.BatchNorm2d(num_features=mid_channels)
